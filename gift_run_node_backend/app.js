2caa6fd02f8ebde06563289d7da08305d78d6740
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-11-02 17:42:43 -0400

    full crud functionality

--- gift_run_node_backend/app.js
+++ gift_run_node_backend/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mongo = require("mongo");
+
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const cors = require("cors");
 
 const indexRoute = require("./routes/index.js");
 
-mongoose.connect("mongodb://localhost/Gift_RUN");
+mongoose
+  .connect("mongodb://localhost/Gift_RUN")
+  .then(console.log("Mongo is connected!"));
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/", indexRoute);
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/run", indexRoute);
 
 app.listen(4000, () => {
   console.log("Gift-RUN Server is gifting!!");
--- gift_run_node_backend/routes/index.js
+++ gift_run_node_backend/routes/index.js
@@ -1,8 +1,74 @@
 const express = require("express");
+const app = express();
 const router = express.Router();
-const axios = require("axios");
-const indexController = require("../controllers/index.js");
+const Run = require("../models/index.js");
 
-router.get("/all-runs", indexController.index);
+//index route
+router.route("/new").post(function(req, res) {
+  const run = new Run(req.body);
+  run
+    .save()
+    .then(run => {
+      res.json("run added!");
+    })
+    .catch(err => {
+      res.status(400).send("something went wrong! Run was not added");
+    });
+});
+
+//create route
+router.route("/").get(function(req, res) {
+  Run.find(function(err, allRuns) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(allRuns);
+    }
+  });
+});
+
+//show route
+router.route("/:id").get(function(req, res) {
+  Run.findById(req.params.id, function(err, aRun) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(aRun);
+    }
+  });
+});
+
+//edit route
+router.route("/:id/edit").get(function(req, res) {
+  Run.findById(req.params.id, function(err, aRun) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(aRun);
+    }
+  });
+});
+
+//update route
+router.route("/:id").put(function(req, res) {
+  Run.findByIdAndUpdate(req.params.id, req.body, function(err, aRun) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(aRun);
+    }
+  });
+});
+
+//Destroy route
+router.route("/:id").delete(function(req, res) {
+  Run.findByIdAndRemove(req.params.id, function(err, deleteRun) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(deleteRun);
+    }
+  });
+});
 
 module.exports = router;
